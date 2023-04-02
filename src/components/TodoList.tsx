d7bd6ff4cfbc20aa3adc939fa8f54fa4951d29f8
humzah alkindi <humzahalkindi@hotmail.com>
2023-04-02 18:00:13 +1000

    added beautiful dnd library

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,35 +2,47 @@ import React from 'react'
 import Todo from './model'
 import SingleTodo from './SingleTodo';
 import "./styles.css"
+import { Droppable } from 'react-beautiful-dnd';
 
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+  completedTodos: Todo[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 
 }
 
-const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos, completedTodos, setCompletedTodos }) => {
   return (
     <div className="container">
-      <div className="todos">
-        <span className="todos__heading">
-          Active Tasks
-        </span>
-        {todos.filter((todo) =>
-          !todo.isDone === true).map((todo) => (
-            <SingleTodo
-              todo={todo} todos={todos} setTodos={setTodos}
-            />))}
-      </div>
-      <div className="todos remove">
-        <span className="todos__heading">
-          Completed Tasks
-        </span>
-        {todos.filter((todo) => todo.isDone === true).map((todo) => (
-          <SingleTodo
-            todo={todo} todos={todos} setTodos={setTodos}
-          />))}
-      </div>
+      <Droppable droppableId='TodosList'>
+        {
+          (provided) => (<div className="todos" ref={provided.innerRef} {...provided.droppableProps}>
+            <span className="todos__heading">
+              Active Tasks
+            </span>
+            {todos.map((todo) => (
+                <SingleTodo
+                  todo={todo} todos={todos} setTodos={setTodos}
+                />))}
+          </div>)
+        }
+      </Droppable>
+      <Droppable droppableId='TodosRemove'>
+        {
+          (provided) => (
+            <div className="todos remove" ref={provided.innerRef} {...provided.droppableProps}>
+              <span className="todos__heading">
+                Completed Tasks
+              </span>
+              {completedTodos.map((todo) => (
+                <SingleTodo
+                  todo={todo} todos={todos} setTodos={setCompletedTodos}
+                />))}
+            </div>
+          )
+        }
+      </Droppable>
     </div>
   )
 }
